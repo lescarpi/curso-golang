3751251a06b903c53eeb6629b23e1866c265c808
Leandro Scarpi <leandroscarpigranelli@gmail.com>
2023-07-05 17:56:34 -0300

    mostra versão

--- hello.go
+++ hello.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	version := 1.1
+	fmt.Println("Olá, este programa está na versão:", version)
+
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("0 - Sair do Programa")
+
+	var comando int
+	fmt.Scan(&comando)
+	fmt.Println("O comando escolhido foi", comando)
 
 }
