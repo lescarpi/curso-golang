4eb83468827a4da482d1d969cb883d32bf7df305
Leandro Scarpi <leandroscarpigranelli@gmail.com>
2023-07-10 20:46:33 -0300

    Criadas funções de monitoramento dos sites

--- hello.go
+++ hello.go
@@ -1,18 +1,72 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+)
 
 func main() {
 
+	exibeIntroducao()
+	for {
+		exibeMenu()
+
+		comando := leComando()
+
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo Logs...")
+		case 0:
+			fmt.Println("Saindo do Programa...")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando!")
+			os.Exit(-1)
+		}
+	}
+
+}
+
+func exibeIntroducao() {
 	version := 1.1
 	fmt.Println("Olá, este programa está na versão:", version)
+}
 
+func exibeMenu() {
 	fmt.Println("1 - Iniciar Monitoramento")
 	fmt.Println("2 - Exibir Logs")
 	fmt.Println("0 - Sair do Programa")
+}
 
-	var comando int
-	fmt.Scan(&comando)
-	fmt.Println("O comando escolhido foi", comando)
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi", comandoLido)
 
+	return comandoLido
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	sites := []string{"https://google.com", "https://www.alura.com.br", "https://twitter.com", "https://www.amazon.com"}
+	for i := 1; i <= 5; i++ {
+		fmt.Println("Teste número", i)
+		for _, site := range sites {
+			testarSite(site)
+		}
+		time.Sleep(5 * time.Second)
+	}
+}
+
+func testarSite(site string) {
+	resp, _ := http.Get(site)
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+	}
 }
