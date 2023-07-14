47ccaaeb71420635176b743d7e6b742415000552
Leandro Scarpi Granelli <leandroscarpigranelli@gmail.com>
2023-07-14 17:48:01 -0300

    alterado pasta projeto monitoramento, criado pasta e primeiro commit projeto banco (continuação curso de go)

---
+++ banco/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var titular string = "Guilherme"
+	var numeroAgencia int = 589
+	var numeroConta int = 123456
+	var saldo float64 = 125.50
+
+	fmt.Println(titular, numeroAgencia, numeroConta, saldo)
+}
