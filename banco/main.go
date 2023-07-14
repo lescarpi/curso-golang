df9e2af2de150bfb98cbfd858f735df09ad4cdd9
Leandro Scarpi Granelli <leandroscarpigranelli@gmail.com>
2023-07-14 18:39:23 -0300

    criado struct contacorrente e metodo sacar

--- banco/main.go
+++ banco/main.go
@@ -2,11 +2,36 @@ package main
 
 import "fmt"
 
-func main() {
-	var titular string = "Guilherme"
-	var numeroAgencia int = 589
-	var numeroConta int = 123456
-	var saldo float64 = 125.50
+type ContaCorrente struct {
+	titular       string
+	numeroAgencia int
+	numeroConta   int
+	saldo         float64
+}
 
-	fmt.Println(titular, numeroAgencia, numeroConta, saldo)
+func (c *ContaCorrente) Sacar(valor float64) string {
+	if valor <= 0 {
+		return "Valor do saque precisa ser maior do que 0!"
+	}
+	if valor <= c.saldo {
+		c.saldo -= valor
+		return "Saque realizado com sucesso!"
+	} else {
+		return "Saldo insuficiente!"
+	}
+}
+
+func main() {
+
+	contaDoGuilherme := ContaCorrente{"Guilherme",
+		589,
+		123456,
+		125.50}
+
+	fmt.Println(contaDoGuilherme)
+
+	fmt.Println(contaDoGuilherme.Sacar(25.50))
+
+	fmt.Println(contaDoGuilherme)
+
 }
