0a0cfa6b145ce9ffdfa880c46d421b196379db1b
Leandro Scarpi Granelli <leandroscarpigranelli@gmail.com>
2023-07-16 16:09:32 -0300

    criado package contas

--- banco/main.go
+++ banco/main.go
@@ -1,46 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
-
-type ContaCorrente struct {
-	titular       string
-	numeroAgencia int
-	numeroConta   int
-	saldo         float64
-}
-
-func (c *ContaCorrente) Sacar(valor float64) string {
-	if valor <= 0 {
-		return "Valor do saque precisa ser maior do que 0!"
-	}
-	if valor > c.saldo {
-		return "Saldo insuficiente!"
-	}
-	c.saldo -= valor
-	return "Saque realizado com sucesso!"
-}
-
-func (c *ContaCorrente) Depositar(valor float64) string {
-	if valor <= 0 {
-		return "Valor do depósito precisa ser maior do que 0!"
-	}
-	c.saldo += valor
-	return "Depósito realizado com sucesso!"
-}
-
-func (c *ContaCorrente) Transferir(valor float64, r *ContaCorrente) string {
-	if valor <= 0 {
-		return "Valor da transferência precisa ser maior do que 0!"
-	}
-	if valor > c.saldo {
-		return "Saldo insuficiente!"
-	}
-	c.Sacar(valor)
-	r.Depositar(valor)
-	return "Transferência realizada com sucesso!"
-}
+import "fmt"
 
 func main() {
 
